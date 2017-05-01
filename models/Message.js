c148b32d8ae50bc4ba9e86143d63ae80e9fc0ae6
Yaraslau Kurmyza <lotask@gmail.com>
2017-05-01 22:37:22 +0200

    Better request parsing: message_changed subtype

--- models/Message.js
+++ models/Message.js
@@ -8,25 +8,31 @@ const messageSchema = mongoose.Schema({
   ts: String,
   username: String,
   text: String,
-  meta: Object,
+  meta: {
+    icons: Object,
+    event_id: String
+  },
   attachments: [Object]
 });
 
 messageSchema.statics.newFromSlackRequest = function(slackReq) {
   const Message = this;
 
+  const event = slackReq.event;
+
   return new Message({
       team_id: slackReq.team_id,
       api_app_id: slackReq.api_app_id,
       event_time: slackReq.event_time,
-      channel: slackReq.event.channel || null,
-      ts: slackReq.event.ts || null,
-      username: slackReq.event.user || slackReq.event.username,
-      text: slackReq.event.text,
+      channel: event.channel || null,
+      ts: event.ts || null,
+      username: event.message ? event.message.username : (event.user || event.username),
+      text: event.message ? event.message.text : event.text,
       meta: {
-        icons: slackReq.event.icons || {}
+        icons: event.icons || {},
+        event_id: slackReq.event_id
       },
-      attachments: slackReq.event.attachments
+      attachments: event.message ? event.message.attachments : event.attachments
     });
 }
 

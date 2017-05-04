9f86318f4b1dc69e62dd3d4a44debed80dee92b3
Yaraslau Kurmyza <lotask@gmail.com>
2017-05-05 00:20:55 +0200

    This is not needed

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ class App extends Component {
           {/* <Nav /> */}
 
           <Route exact path="/" component={Messages} />
-          <Route path="/messages" component={Messages} />
           <Route path="/about" component={About} />
         </Container>
       </Router>

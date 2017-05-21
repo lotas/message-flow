35476b7504b46128d8a71e94d2116e47cf2889fc
Yaraslau Kurmyza <lotask@gmail.com>
2017-05-21 12:04:44 +0200

    Catching network errors

--- src/api.js
+++ src/api.js
@@ -1,22 +1,41 @@
 import config from './config';
 
+const commonFetchOptions = {
+    credentials: 'include'
+};
+
+const checkStatus = (res) => {
+    if (res.status === 401) {
+        throw new Error('Login required');
+    } else if (res.status === 403) {
+        throw new Error('Authorization fail');
+    }
+
+    return res;
+}
+
+const fetchWithCatch = (...args) => {
+    return fetch(...args)
+            .then(checkStatus)
+            .then(res => res.json())
+            .catch((err) => {
+                console.error('Network call failed:', err)
+            });
+}
+
+
 export const getMessages = (limit=10, skip=0) => {
-   return fetch(`${config.apiUrl}/messages/?limit=${limit}&skip=${skip}`, {
-            credentials: 'include'
-        })
-        .then(res => res.json());
+   return fetchWithCatch(
+       `${config.apiUrl}/messages/?limit=${limit}&skip=${skip}`,
+        commonFetchOptions
+    );
 }
 
 export const getStats = () => {
-    return fetch(`${config.apiUrl}/messages/stats`, {
-                credentials: 'include'
-            })
-            .then(res => res.json());
+    return fetchWithCatch(`${config.apiUrl}/messages/stats`,
+        commonFetchOptions);
 }
 
 export const getAuthUser = () => {
-    return fetch(`${config.apiUrl}/auth/info`, {
-                credentials: 'include'
-            })
-            .then(res => res.json());
+    return fetchWithCatch(`${config.apiUrl}/auth/info`, commonFetchOptions);
 }
